9b35878e28339b72c24328453998590cf08bfbf8
Sean Alden <sean.alden@great.sch.id>
2026-03-27 15:08:42 +0700

    add promo code logic #3

--- resources/views/emails/promo_code.blade.php
+++ resources/views/emails/promo_code.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>SolHer Promo Code</title>
+    <title>Solher Promo Code</title>
 </head>
 <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; background-color: #f9f9f9; padding: 20px;">
     <div style="max-width: 600px; margin: 0 auto; background-color: #fff; padding: 40px; border: 1px solid #eee; border-radius: 10px;">
@@ -9,7 +9,7 @@
         <hr style="border: none; border-top: 1px solid #eaeaea; margin: 30px 0;">
 
         <h3 style="color: #222;">Hello there!</h3>
-        <p style="color: #555;">Thank you for subscribing to SolHer. As promised, here is your exclusive promo code for your first order:</p>
+        <p style="color: #555;">Thank you for subscribing to Solher. As promised, here is your exclusive promo code for your first order:</p>
 
         <div style="text-align: center; margin: 40px 0;">
             <span style="background-color: #111; color: #fff; padding: 15px 35px; font-size: 24px; font-weight: bold; letter-spacing: 5px; border-radius: 4px;">
@@ -22,7 +22,7 @@
         </p>
 
         <br>
-        <p style="color: #555;">Happy Shopping,<br><strong style="color: #111;">SolHer Team</strong></p>
+        <p style="color: #555;">Happy Shopping,<br><strong style="color: #111;">Solher Team</strong></p>
     </div>
 </body>
 </html>
