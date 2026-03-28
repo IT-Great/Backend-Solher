d2b7500ac25d1813bcf1e8efa43b756edf877e0c
Sean Alden <sean.alden@great.sch.id>
2026-03-28 10:06:09 +0700

    update domain in sanctum

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,solher.co.id',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,solher.co.id,back.solher.co.id',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
