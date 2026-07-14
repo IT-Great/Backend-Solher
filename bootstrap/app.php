65a9503ccfbb8504d2b7c5c51ce4a8b2b962a2ed
Sean Alden <sean.alden@great.sch.id>
2026-07-14 15:12:06 +0700

    add laravel sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -45,4 +46,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+        Integration::handles($exceptions);
     })->create();
